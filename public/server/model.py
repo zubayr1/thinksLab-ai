463cb54a35dc807371d8a26fb9d6f85993666a61
Khabbab Zakaria <46716277+KhabbabZakaria@users.noreply.github.com>
2023-12-29 07:24:22 +0100

    Update model.py

--- public/server/model.py
+++ public/server/model.py
@@ -4,15 +4,6 @@ import openai
 openai.api_key = Config.OPENAI_API
 
 def model(prompt):
-    '''model = "text-davinci-003"
-    completions = openai.Completion.create(
-        engine=model,
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )'''
     completions = openai.ChatCompletion.create(
     model="gpt-3.5-turbo", 
     messages=[{"role": "user", "content": prompt}]
