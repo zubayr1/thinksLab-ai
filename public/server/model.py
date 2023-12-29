356ca886427848cfa2fb2814a99c4bb1fd736706
Khabbab Zakaria <46716277+KhabbabZakaria@users.noreply.github.com>
2023-12-29 07:39:18 +0100

    Update model.py

--- public/server/model.py
+++ public/server/model.py
@@ -1,8 +1,7 @@
 from config import Config
 import openai
 
-#openai.api_key = Config.OPENAI_API
-openai.api_key = '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'
+openai.api_key = Config.OPENAI_API
 
 def model(prompt):
     completions = openai.ChatCompletion.create(
