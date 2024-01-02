659acffe1d4cecee94cc0371316e7ea1a9c35ed3
Khabbab Zakaria <46716277+KhabbabZakaria@users.noreply.github.com>
2024-01-02 10:15:53 +0100

    Update Chatbot.js

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -25,12 +25,10 @@ function Chatbot({email}) {
   if (process.env.REACT_APP_NODE_ENV==='docker'){
       const baseURL = 'http://backend:5000'; 
   }
+  elif (process.env.REACT_APP_NODE_ENV === 'production'){
+    const baseURL = `http://${process.env.REACT_APP_PROD_IP}:5000`;
+  }
   
-  const baseURL = process.env.REACT_APP_NODE_ENV === 'production' ?
-   `http://${process.env.REACT_APP_PROD_IP}:5000` :
-   'http://127.0.0.1:5000';
-
-  const baseURL = 
   
   const [question, setQuestion] = useState('');
 
