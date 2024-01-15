51c81010643dbd5ba4ddadab81b19a84d3ef088f
khaled-ibt <134131056+khaled-ibt@users.noreply.github.com>
2024-01-15 14:02:20 +0000

    Update Chatbot.js

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -25,7 +25,7 @@ import { onAuthStateChanged } from "firebase/auth";
 
 function Chatbot({email}) {
 
-  let baseURL = 'http://127.0.0.1:5002';
+  let baseURL = 'http://thinklabs-backend:5002';
   
 
   // if (process.env.REACT_APP_NODE_ENV === 'dockerportclose') {
