1e893ddf33a25f4650328dc454b7f6abfae04b8a
zubayr1 <write2zubayr@gmail.com>
2024-01-22 00:30:45 +0100

    landing do

--- src/components/LandingDo.js
+++ src/components/LandingDo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Grid, Image, Divider } from 'semantic-ui-react'
+import { Grid, Image, Divider, Button } from 'semantic-ui-react'
 
 import normal_chat from '../assets/empty_chat.svg';
 import intent_recognition from '../assets/empty_chat2.svg';
@@ -79,8 +79,16 @@ function LandingDo() {
         marginTop: '1%',
       };
 
+
+      
+const handlebutton = async () =>
+{
+    
+    
+}
+
   return (
-    <div style={{marginTop: '4%', backgroundColor:'#F4F5F7'}}>
+    <div style={{marginTop: '4%', backgroundColor:'#F4F5F7', paddingBottom:'2%'}}>
 
         <div style={{paddingTop: '8%',marginLeft:'8%', marginRight:'8%'}}>
 
@@ -103,8 +111,16 @@ function LandingDo() {
                             <p style={paragraphStyle}>✔ Enhance your applications with expert assistance in crafting compelling personal statements and CVs.</p>
                             <p style={paragraphStyle}>✔ Explore career opportunities and unlock your maximum potential.</p>
                         </div>
-                        
+
+                        <div style={{display:'flex', alignItems:'center', justifyContent:'center', marginTop:'1%'}}>
+                            <Button style={{background: 'linear-gradient(to right, #2971ea, #1b4aee)', color:'white', 
+                                paddingLeft: '7%', paddingRight: '7%', fontFamily: 'Montserrat'}} 
+                                onClick={handlebutton} size='huge' >Join Waitlist</Button>
+                        </div>
                     </Grid.Column>
+
+
+                    
 
 
                 </Grid.Row>
@@ -117,6 +133,12 @@ function LandingDo() {
                             <p style={paragraphStyle}>✔ Enjoy consistent and seamless interactions with our platform's immunity to unintentional responses.</p>
                             <p style={paragraphStyle}>✔ Receive valuable information on scholarships, financial aid, and part-time jobs.</p>
                             <p style={paragraphStyle}>✔ Gain insights into skill development, internships, and job opportunities for a well-rounded journey. </p>
+                        </div>
+
+                        <div style={{display:'flex', alignItems:'center', justifyContent:'center', marginTop:'2%'}}>
+                            <Button style={{background: 'linear-gradient(to right, #2971ea, #1b4aee)', color:'white', 
+                                paddingLeft: '7%', paddingRight: '7%', fontFamily: 'Montserrat'}} 
+                                onClick={handlebutton} size='huge' >Join Waitlist</Button>
                         </div>
                     </Grid.Column>
                     
@@ -143,7 +165,7 @@ function LandingDo() {
                     </div>
                 </Grid.Row>
 
-                <Grid.Row only='tablet'>
+                <Grid.Row only='tablet' centered>
                     <div>
                         <p style={headingStyle_tablet}>Enhance your chances to launch your dream career</p>
                         <p style={paragraphStyle_tablet}>✔ Receive guidance in choosing the ideal course, university, or job tailored to your unique profile.</p>
@@ -151,14 +173,26 @@ function LandingDo() {
                         <p style={paragraphStyle_tablet}>✔ Explore career opportunities and unlock your maximum potential.</p>
                     </div>
 
+                    <div style={{ marginTop:'2%', width:'50%'}}>
+                        <Button style={{background: 'linear-gradient(to right, #2971ea, #1b4aee)', color:'white', 
+                            paddingLeft: '7%', paddingRight: '7%', fontFamily: 'Montserrat'}} 
+                            onClick={handlebutton} size='huge' >Join Waitlist</Button>
+                    </div>
+
                 </Grid.Row>
 
-                <Grid.Row only='mobile'>
+                <Grid.Row only='mobile' centered>
                     <div>
                         <p style={headingStyle_mobile}>Enhance your chances to launch your dream career</p>
                         <p style={paragraphStyle_mobile}>✔ Receive guidance in choosing the ideal course, university, or job tailored to your unique profile.</p>
                         <p style={paragraphStyle_mobile}>✔ Enhance your applications with expert assistance in crafting compelling personal statements and CVs.</p>
                         <p style={paragraphStyle_mobile}>✔ Explore career opportunities and unlock your maximum potential.</p>
+                    </div>
+
+                    <div style={{ marginTop:'2%', width:'50%'}}>
+                        <Button style={{background: 'linear-gradient(to right, #2971ea, #1b4aee)', color:'white', 
+                            paddingLeft: '7%', paddingRight: '7%', fontFamily: 'Montserrat'}} 
+                            onClick={handlebutton} size='huge' >Join Waitlist</Button>
                     </div>
 
                 </Grid.Row>
@@ -175,7 +209,7 @@ function LandingDo() {
 
                 </Grid.Row>
 
-                <Grid.Row only='tablet'>
+                <Grid.Row only='tablet' centered>
                     <div>
                         <p style={headingStyle_tablet}>Seamless User Engagement with Immunity Technology</p>
                         <p style={paragraphStyle_tablet}>✔ Enjoy consistent and seamless interactions with our platform's immunity to unintentional responses.</p>
@@ -183,14 +217,26 @@ function LandingDo() {
                         <p style={paragraphStyle_tablet}>✔ Gain insights into skill development, internships, and job opportunities for a well-rounded journey. </p>
                     </div>
 
+                    <div style={{ marginTop:'2%', width:'50%'}}>
+                        <Button style={{background: 'linear-gradient(to right, #2971ea, #1b4aee)', color:'white', 
+                            paddingLeft: '7%', paddingRight: '7%', fontFamily: 'Montserrat'}} 
+                            onClick={handlebutton} size='huge' >Join Waitlist</Button>
+                    </div>
+
                 </Grid.Row>
 
-                <Grid.Row only='mobile'>
+                <Grid.Row only='mobile' centered>
                     <div>
                         <p style={headingStyle_mobile}>Seamless User Engagement with Immunity Technology</p>
                         <p style={paragraphStyle_mobile}>✔ Enjoy consistent and seamless interactions with our platform's immunity to unintentional responses.</p>
                         <p style={paragraphStyle_mobile}>✔ Receive valuable information on scholarships, financial aid, and part-time jobs.</p>
                         <p style={paragraphStyle_mobile}>✔ Gain insights into skill development, internships, and job opportunities for a well-rounded journey. </p>
+                    </div>
+
+                    <div style={{ marginTop:'2%', width:'50%'}}>
+                        <Button style={{background: 'linear-gradient(to right, #2971ea, #1b4aee)', color:'white', 
+                            paddingLeft: '7%', paddingRight: '7%', fontFamily: 'Montserrat'}} 
+                            onClick={handlebutton} size='huge' >Join Waitlist</Button>
                     </div>
 
                 </Grid.Row>
